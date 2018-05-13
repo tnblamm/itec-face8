c900a55a1c35d3b4d7bb9b6375da5fd3ee2b65ef
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-05-13 16:57:22 +0700

    commit

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    public apiHost = "https://iteccyle8.herokuapp.com//api";
+    public apiHost = "https://iteccyle8.herokuapp.com/api";
     public host = "https://iteccyle8.herokuapp.com/";
 }
