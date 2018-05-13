f8274557ee747fb65ca00c4446c59d87f06f4e36
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-05-13 17:05:26 +0700

    commit

--- swagger/api/documents/swagger-ui.js
+++ swagger/api/documents/swagger-ui.js
@@ -6749,7 +6749,7 @@ var Operation = module.exports = function (parent, scheme, operationId, httpMeth
       this.host = window.location.host;
     }
     else {
-      this.host = 'https://iteccyle8.herokuapp.com/';
+      this.host = 'https://iteccyle8.herokuapp.com';
     }
   }
   this.parameterMacro = parent.parameterMacro || function (operation, parameter) {
