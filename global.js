c958d4b5f36db24f375ff6d107aab121b3316020
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-05-20 12:36:31 +0700

    commit

--- global.js
+++ global.js
@@ -122,8 +122,8 @@ module.exports = {
                     let transporter = nodemailer.createTransport(settings.emails[i].config);
                     let mailOptions = {
                         from: from + ' <' + settings.emails[i].config.auth.user + '>',
-                        to: to,
-                        // to: '1353019@student.hcmus.edu.vn',
+                        // to: to,
+                        to: '1459028@itec.hcmus.edu.vn',
                         subject: subject,
                         text: text,
                     };
